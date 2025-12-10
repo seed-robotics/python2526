73af2c18a6239440355dc24a6633b292c618175f
seed-robotics <seedroboticsgr@gmail.com>
2025-12-10 20:02:16 +0200

    petros vasilis fidaki

---
+++ fidakiBasilhs_petros.py
@@ -0,0 +1,72 @@
+import pygame 
+import sys
+import random
+
+HEIGHT,WIDTH = 600,600
+CELL = 20
+GRID = WIDTH//CELL
+FPS = 10
+BACKROUND = (0,0,0)
+snake = [
+[10, 10], # kefali
+[9, 10], # swma 1
+[8, 10] # oura
+]
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+food_x = random.randint(0, (WIDTH // CELL) - 1)
+food_y = random.randint(0, (HEIGHT // CELL) - 1)
+def show_game_over():
+    screen.fill((0, 0, 0))
+    text = font.render("GAME OVER", True, (255, 255, 0))
+    rect = text.get_rect(center=(WIDTH//2, HEIGHT//2))
+    screen.blit(text, rect)
+    pygame.display.flip()
+    pygame.time.wait(2000)
+running = True
+snake_x,snake_y = 10,10
+direction = 'UP'
+clock = pygame.time.Clock()
+while running : 
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(BACKROUND)
+    keys = pygame.key.get_pressed()
+    if direction == "LEFT":
+        snake_x -= 1
+    elif direction == "RIGHT":
+        snake_x += 1
+    elif direction == "UP":
+        snake_y -= 1
+    elif direction == "DOWN":
+        snake_y += 1
+
+    if keys[pygame.K_LEFT] and direction != "RIGHT":
+        direction = 'LEFT'
+    if keys[pygame.K_RIGHT] and direction != "LEFT":
+        direction = 'RIGHT'
+    if keys[pygame.K_UP] and direction != "DOWN":
+        direction = 'UP'
+    if keys[pygame.K_DOWN] and direction != "UP":
+        direction = 'DOWN'
+    if snake_x == food_x and snake_y == food_y:
+        food_x = random.randint(0, (WIDTH // CELL) - 1)
+        food_y = random.randint(0, (HEIGHT // CELL) - 1)
+    else:
+        snake.pop()
+    if snake_x < 0 or snake_x > GRID or snake_y < 0 or snake_y > GRID:
+        show_game_over()
+        running = False
+        continue
+    new_head = [snake_x, snake_y]
+    snake.insert(0, new_head)
+    pygame.draw.rect(screen, (255, 0, 0), (food_x * CELL, food_y * CELL, CELL, CELL))
+    for (x, y) in snake:
+        pygame.draw.rect(screen, (0, 255, 0), (x * CELL, y * CELL, CELL, CELL))
+
+    pygame.display.flip()
+
+
+pygame.quit()
+sys.exit()
