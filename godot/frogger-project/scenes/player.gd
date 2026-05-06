c34786b4ce7c939b79f31be787e224e5449b494f
seed-robotics <seedroboticsgr@gmail.com>
2026-05-06 21:00:40 +0300

    added animations

--- godot/frogger-project/scenes/player.gd
+++ godot/frogger-project/scenes/player.gd
@@ -13,8 +13,22 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
+	animation()
 	move_and_slide()
 	
 	if Input.is_action_just_released("space"):
 		print("to binteo einai 2.55.10")   
-	
+func animation():
+	if direction:
+		if direction.x != 0 and direction.y == 0: 
+			$AnimatedSprite2D.animation = 'left_right'
+			$AnimatedSprite2D.flip_h = direction.x > 0
+		if direction.y < 0 and direction.x == 0:
+			$AnimatedSprite2D.animation = 'up'
+		if direction.y > 0 and direction.x == 0:
+			$AnimatedSprite2D.animation = 'down'
+		if direction.x != 0 and direction.y != 0:
+			$AnimatedSprite2D.animation = 'left_right'
+			$AnimatedSprite2D.flip_h = direction.x > 0
+	else:
+		$AnimatedSprite2D.frame = 0
